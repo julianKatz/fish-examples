dda6b68ae3e2dcd907209e1c8cbc935899813407
juliankatz <juliankatz@google.com>
2022-08-18 14:15:18 -0700

    generate-pods.go

---
+++ generate-pods.go
@@ -0,0 +1,55 @@
+package main
+
+import (
+	"fmt"
+	"html/template"
+	"os"
+	"strconv"
+)
+
+const podString = `apiVersion: v1
+kind: Pod
+metadata:
+  name: {{ .Name }}
+  namespace: default
+spec:
+  containers:
+  - name: webserver
+    image: pizza:latest
+    ports:
+    - containerPort: 80
+  - name: webwatcher
+    image: taco:latest`
+
+var templ *template.Template
+
+func init() {
+	// template.Must takes the reponse of template.ParseFiles and does error checking
+	temp, err := template.New("bla").Parse(podString)
+	if err != nil {
+		panic(err)
+	}
+	templ = temp
+}
+
+type data struct {
+	Name string
+}
+
+const numberOfPods = 10
+
+func main() {
+	for i := 0; i < numberOfPods; i++ {
+		// standard output to print merged data
+		err := templ.Execute(os.Stdout, data{Name: "pod" + strconv.Itoa(i)})
+		// if there is no error,
+		// prints the output
+		if err != nil {
+			fmt.Println(err)
+		}
+
+		if i < numberOfPods-1 {
+			fmt.Println("\n---")
+		}
+	}
+}
